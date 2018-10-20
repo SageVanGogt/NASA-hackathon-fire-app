bff43e602d0bad853b7b500cceb9290db6aa2fe3
Marika Ross <marika.ross@gmail.com>
2018-10-20 13:11:57 -0600

    Delete template to only display Landing component

--- src/App.js
+++ src/App.js
@@ -4,45 +4,13 @@ import Landing from './components/Landing';
 import styled from 'styled-components';
 import './App.css';
 
-const Page = ({ title }) => (
-    <div className="App">
-      <div className="App-header">
-      </div>
-      <p className="App-intro">
-        This is the {title} page.
-      </p>
-      <p>
-        <Link to="/">Home</Link>
-      </p>
-      <p>
-        <Link to="/about">About</Link>
-      </p>
-      <p>
-        <Link to="/settings">Settings</Link>
-      </p>
-    </div>
-);
 
-const Home = (props) => (
-  <Page title="Home"/>
-);
-
-const About = (props) => (
-  <Page title="About"/>
-);
-
-const Settings = (props) => (
-  <Page title="Settings"/>
-);
 
 class App extends Component {
   render() {
     return (
       <div className='app'>
-        <Landing />
-        <Route path="/" component={Home}/>
-        <Route path="/about" component={About}/>
-        <Route path="/settings" component={Settings}/>
+        <Route path="/" component={Landing}/>
       </div>
     );
   }
