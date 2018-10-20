b8e08796186f93ea7ce7d70fc79fbe7abf6cbf66
Christie Lynam <christielynam@gmail.com>
2018-10-20 12:54:17 -0600

    Add styles to landing page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Landing from './components/Landing';
+import styled from 'styled-components';
 import './App.css';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
         <Landing />
       </div>
     );
