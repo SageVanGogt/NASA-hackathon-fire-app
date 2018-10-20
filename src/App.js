089feae92f1fd76583ce0a450534b941645d74a3
Marika Ross <marika.ross@gmail.com>
2018-10-20 17:40:56 -0600

    Change route for instructional page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Route, Link } from "react-router-dom";
 import Landing from './components/Landing';
+import InTheEventOfAFire from './components/inTheEventOfAFire';
 import FireMap from './components/FireMap/FireMap';
 import styled from 'styled-components';
 import './App.css';
@@ -10,8 +11,13 @@ class App extends Component {
   render() {
     return (
       <div className='app'>
+<<<<<<< HEAD
         <Route exact path="/" component={Landing} />
         <Route path='/fire-map' component={FireMap} />
+=======
+        <Route exact path="/" component={Landing}/>
+        <Route path="/ReportedFire" component={InTheEventOfAFire}/>
+>>>>>>> Change route for instructional page
       </div>
     );
   }
