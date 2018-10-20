300f359deeb5f138072aed844e567f7ebdbd4391
Christie Lynam <christielynam@gmail.com>
2018-10-20 11:44:11 -0600

    Create components folder and Landing.js

---
+++ src/components/Landing.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+const Landing = () => {
+  return (
+    <div>
+      <h1></h1>
+      <button>Report a Fire</button>
+      <button>Find a Fire</button>
+    </div>
+  )
+
+}
+
+export default Landing
