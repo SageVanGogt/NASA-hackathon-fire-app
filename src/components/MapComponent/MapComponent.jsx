f1d0960bfa88ca570ef4ff25b868c74d04c5b2d4
Sage Vogt <thurmanvogt@gmail.com>
2018-10-20 15:16:15 -0600

    Add mapComponent boilerplate with imports and google map component

--- src/components/MapComponent/MapComponent.jsx
+++ src/components/MapComponent/MapComponent.jsx
@@ -1,1 +1,19 @@
 
+import React from 'react';
+import { withScriptjs, withGoogleMap, GoogleMap, Marker }
+  from 'react-google-maps';
+import { InfoBox } from "react-google-maps/lib/components/addons/InfoBox";
+
+export const MapComponent = () => {
+
+
+  return (
+    <GoogleMap
+      defaultZoom={16}
+      center={position}>
+      {markers}
+    </GoogleMap>
+  );
+};
+
+export default MapComponent;
