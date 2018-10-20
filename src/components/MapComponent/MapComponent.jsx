f21e84501968ec1bba41e089b4a10f1e1ad3bb3f
Sage Vogt <thurmanvogt@gmail.com>
2018-10-20 15:18:43 -0600

    Add iterative functionality to create many markers and infoboxs from imported fire location data

--- src/components/MapComponent/MapComponent.jsx
+++ src/components/MapComponent/MapComponent.jsx
@@ -4,9 +4,32 @@ import { withScriptjs, withGoogleMap, GoogleMap, Marker }
   from 'react-google-maps';
 import { InfoBox } from "react-google-maps/lib/components/addons/InfoBox";
 
-export const MapComponent = () => {
-
-
+export const MapComponent = withScriptjs(withGoogleMap((
+  { position, markerCoords, toggleShowing, ...props }
+) => {
+  const markers = markerCoords.map((location, index) => {
+    let { lat, lng } = location;
+    lat = parseFloat(lat);
+    lng = parseFloat(lng);
+    return (
+      <Marker
+        key={`mapmarker-${index}`}
+        onClick={() => toggleShowing(location)}
+        position={{lat: lat, lng: lng}}>
+        {
+          location.isShowing === true && 
+          <InfoBox
+            options={{ closeBoxURL: ``, enableEventPropagation: true }}>
+            <div className="info-box">
+              <div className="info-text-box">
+                {/* <h2>{what we want to display in info box}</h2> */}
+              </div>
+            </div>
+          </InfoBox>
+        }
+      </Marker>
+    );
+  });
   return (
     <GoogleMap
       defaultZoom={16}
@@ -14,6 +37,6 @@ export const MapComponent = () => {
       {markers}
     </GoogleMap>
   );
-};
+}));
 
 export default MapComponent;
