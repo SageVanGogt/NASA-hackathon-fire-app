0643da7b4ea507cb5630674aac11829d2fae89a2
Christie Lynam <christielynam@gmail.com>
2018-10-20 11:57:37 -0600

    Add fire img

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 const Landing = () => {
   return (
     <div>
-      <h1></h1>
+      <h1>FIRE!!!</h1>
       <button>Report a Fire</button>
       <button>Find a Fire</button>
     </div>
